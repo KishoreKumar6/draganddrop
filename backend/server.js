2bc77aca5cc1b9073cdb8f005c9926f21e0ecf8e
kishorekumar6 <kishoreramu20@gmail.com>
2025-06-25 19:00:59 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -1,81 +1,77 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const { Server } = require('socket.io');
-const http = require('http');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const http = require("http");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://kishorekumar20101999:1Bg3cvGVAY8PV9Er@cluster0.yalqjox.mongodb.net/', {
-  dbName: 'gridApp'
-});
+mongoose.connect(
+  "mongodb+srv://kishorekumar20101999:1Bg3cvGVAY8PV9Er@cluster0.yalqjox.mongodb.net/",
+  {
+    dbName: "gridApp",
+  }
+);
 
 const BoxSchema = new mongoose.Schema({
-    index: Number,
-    value: String,
-    owner: String,
+  index: Number,
+  value: String,
+  owner: String,
 });
 
-const Box = mongoose.model('Box', BoxSchema);
+const Box = mongoose.model("Box", BoxSchema);
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: '*' }
+  cors: { origin: "*" },
 });
 
-io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-    socket.on('get-grid', async () => {
-        const boxes = await Box.find();
-        socket.emit('grid-data', boxes);
-    });
+  socket.on("get-grid", async () => {
+    const boxes = await Box.find();
+    socket.emit("grid-data", boxes);
+  });
 
-    socket.on('update-box', async ({ index, value, owner }) => {
-        let box = await Box.findOne({ index });
-        if (!box) {
-            box = new Box({ index, value, owner });
-        } else {
-            if (box.owner !== owner) return;
-            box.value = value;
-        }
-        await box.save();
-        io.emit('grid-data', await Box.find());
-    });
+  socket.on("update-box", async ({ index, value }) => {
+    let box = await Box.findOne({ index });
+    if (!box) {
+      box = new Box({ index, value });
+    } else {
+      box.value = value;
+    }
+    await box.save();
+    io.emit("grid-data", await Box.find());
+  });
 
-    socket.on('drag-drop', async ({ from, to }) => {
-        try {
-            let fromBox = await Box.findOne({ index: from });
-            let toBox = await Box.findOne({ index: to });
+  socket.on("drag-drop", async ({ from, to }) => {
+    const fromBox = await Box.findOne({ index: from });
+    const toBox = await Box.findOne({ index: to });
 
-            if (!fromBox) fromBox = await Box.create({ index: from, value: '', owner: null });
-            if (!toBox) toBox = await Box.create({ index: to, value: '', owner: null });
+    if (!fromBox) return;
 
-            const tempValue = fromBox.value;
-            const tempOwner = fromBox.owner;
+    if (toBox) {
+      const tempValue = fromBox.value;
+      fromBox.value = toBox.value;
+      toBox.value = tempValue;
 
-            fromBox.value = toBox.value;
-            fromBox.owner = toBox.owner;
+      await fromBox.save();
+      await toBox.save();
+    } else {
+      fromBox.index = to;
+      await fromBox.save();
+    }
 
-            toBox.value = tempValue;
-            toBox.owner = tempOwner;
+    io.emit("grid-data", await Box.find());
+  });
 
-            await fromBox.save();
-            await toBox.save();
-
-            const allBoxes = await Box.find();
-            io.emit('grid-data', allBoxes);
-        } catch (error) {
-            console.error('Drag-drop error:', error);
-        }
-    });
-
-    socket.on('clear-all', async () => {
-        await Box.deleteMany();
-        io.emit('grid-data', []);
-    });
+  socket.on("clear-all", async () => {
+    await Box.deleteMany();
+    io.emit("grid-data", []);
+  });
 });
 
-server.listen(4000, () => console.log('Server running on port 4000'));
+server.listen(4000, () => console.log("Server running on port 4000"));
